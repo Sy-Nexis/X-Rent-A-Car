4e6b5f5c6f791fd1481f9c04377435b9354c1cbe
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-30 18:42:24 +0530

    SQL Search & Input Added

---
+++ Backend/src/actions/vehicleActions.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import pool from '../db';
+import { revalidatePath } from '../../../.next/dev/cache';
+
+// Fetch all vehicles
+export async function getVehicles() {
+    try {
+        const [rows] = await pool.query('SELECT * FROM vehicles ORDER BY created_at DESC');
+        return { success: true, data: rows };
+    } catch (error) {
+        console.error("Database Error:", error);
+        return { success: false, error: "Failed to fetch vehicles" };
+    }
+}
+
+
+export async function addVehicle(formData: FormData) {
+    try {
+        const make = formData.get('make');
+        const model = formData.get('model');
+        const licensePlate = formData.get('licensePlate');
+
+        const [result] = await pool.execute(
+            'INSERT INTO vehicles (make, model, license_plate, status) VALUES (?, ?, ?, ?)',
+            [make, model, licensePlate, status]
+        );
+
+        // Next.js --> Clear Cache & Update
+        revalidatePath('/admin');
+
+
+        return { success: true };
+    } catch (error) {
+        console.error("Database Error:", error);
+        return { success: false, error: "Failed to add vehicle" };
+    }
+}
