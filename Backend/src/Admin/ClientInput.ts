1afdc2d50697beebfdec2a3b43364c3365abb3c2
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-05-01 22:29:34 +0530

    Client Add & Update Backend

---
+++ Backend/src/Admin/ClientInput.ts
@@ -0,0 +1,92 @@
+import { Router, Request, Response } from 'express';
+import pool from '../db';
+
+const router = Router();
+
+// /api/client/add
+router.post('/add', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { make, model, year, vin, license_plate, transmission, fuel_type, engine_capacity, color, mileage, daily_rate, branch, status } = req.body;
+        // Secure SQL --> ? -- SQL INJECTION Can't
+        const insertQuery = `INSERT INTO clients ( make, model, year, vin, license_plate, transmission, fuel_type, engine_capacity, color, mileage, daily_rate, branch, status ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        const [result] = await pool.execute(insertQuery, [make, model, year, vin, license_plate, transmission, fuel_type, engine_capacity, color, mileage, daily_rate, branch, status]);
+
+
+
+        res.status(201).json({
+            success: true,
+            message: 'Client successfully registered to the fleet.',
+            data: result
+        });
+
+    } catch (error: any) {
+        console.error('Error inserting client:', error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error while saving client data.'
+        });
+    }
+});
+
+router.put('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { vin } = req.query;
+
+        if (!vin) {
+            res.status(400).json({
+                success: false,
+                message: 'Please provide the client .......'
+            });
+            return;
+        }
+
+        const { make, model, year, licensePlate, transmission, fuelType, engineCapacity, color, mileage, dailyRate, branch, status } = req.body;
+
+
+        //COALESCE(?, nameee) --> if NULL NO UPDATE
+        const updateQuery = `
+            UPDATE clients 
+            SET  make = COALESCE(?, make), model = COALESCE(?, model), year = COALESCE(?, year), license_plate = COALESCE(?, license_plate), transmission = COALESCE(?, transmission), fuel_type = COALESCE(?, fuel_type), engine_capacity = COALESCE(?, engine_capacity), color = COALESCE(?, color), mileage = COALESCE(?, mileage), daily_rate = COALESCE(?, daily_rate), branch = COALESCE(?, branch), status = COALESCE(?, status)
+            WHERE vin = ? `;
+
+        const [result]: any = await pool.execute(updateQuery, [
+            make ?? null,
+            model ?? null,
+            year ?? null,
+            licensePlate ?? null,
+            transmission ?? null,
+            fuelType ?? null,
+            engineCapacity ?? null,
+            color ?? null,
+            mileage ?? null,
+            dailyRate ?? null,
+            branch ?? null,
+            status ?? null,
+            String(vin)
+        ]);
+
+        if (result.affectedRows === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'No client found matching that VIN.'
+            });
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            message: `CLient with ........ has been successfully updated.`
+        });
+
+    } catch (error: any) {
+        console.error('Error updating client:', error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error while attempting to update client data.'
+        });
+    }
+});
+
+export default router;
