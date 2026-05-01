51abe18cd858dc0ea4c4c9e3d2945d65d7fbb792
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-05-01 22:26:05 +0530

    Client View Backend

---
+++ Backend/src/Admin/ClientView.ts
@@ -0,0 +1,55 @@
+import { Router, Request, Response } from 'express';
+import pool from '../db';
+
+const router = Router();
+
+// /api/client/view
+router.get('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM client ORDER BY created_at DESC');
+
+        res.status(200).json({
+            success: true,
+            count: (rows as any[]).length,
+            data: rows
+        });
+
+    } catch (error: any) {
+        console.error('Error inserting client:', error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error while saving client data.'
+        });
+    }
+});
+
+router.get('/:id', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const [rows]: any = await pool.query('SELECT * FROM client WHERE id = ?', [id]);
+
+
+        if (rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'Client not found'
+            });
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            data: rows[0]
+        });
+
+    } catch (error: any) {
+        console.error('Error fetching client details:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+    }
+});
+
+export default router;
