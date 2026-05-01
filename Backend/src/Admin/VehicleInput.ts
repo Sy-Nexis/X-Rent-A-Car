a411578fbf37d4ebce40e1b43fd7f20c0cf9f399
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-05-01 19:24:07 +0530

    Backend Vehicle Update Done

--- Backend/src/Admin/VehicleInput.ts
+++ Backend/src/Admin/VehicleInput.ts
@@ -29,4 +29,64 @@ router.post('/add', async (req: Request, res: Response): Promise<void> => {
     }
 });
 
+router.put('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { vin } = req.query;
+
+        if (!vin) {
+            res.status(400).json({
+                success: false,
+                message: 'Please provide the vehicle VIN'
+            });
+            return;
+        }
+
+        const { make, model, year, licensePlate, transmission, fuelType, engineCapacity, color, mileage, dailyRate, branch, status } = req.body;
+
+
+        //COALESCE(?, nameee) --> if NULL NO UPDATE
+        const updateQuery = `
+            UPDATE vehicles 
+            SET  make = COALESCE(?, make), model = COALESCE(?, model), year = COALESCE(?, year), license_plate = COALESCE(?, license_plate), transmission = COALESCE(?, transmission), fuel_type = COALESCE(?, fuel_type), engine_capacity = COALESCE(?, engine_capacity), color = COALESCE(?, color), mileage = COALESCE(?, mileage), daily_rate = COALESCE(?, daily_rate), branch = COALESCE(?, branch), status = COALESCE(?, status)
+            WHERE vin = ? `;
+
+        const [result]: any = await pool.execute(updateQuery, [
+            make ?? null,
+            model ?? null,
+            year ?? null,
+            licensePlate ?? null,
+            transmission ?? null,
+            fuelType ?? null,
+            engineCapacity ?? null,
+            color ?? null,
+            mileage ?? null,
+            dailyRate ?? null,
+            branch ?? null,
+            status ?? null,
+            String(vin)
+        ]);
+
+        if (result.affectedRows === 0) {
+            res.status(404).json({
+                success: false,
+                message: 'No vehicle found matching that VIN.'
+            });
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            message: `Vehicle with VIN ${vin} has been successfully updated.`
+        });
+
+    } catch (error: any) {
+        console.error('Error updating vehicle:', error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error while attempting to update vehicle data.'
+        });
+    }
+});
+
 export default router;
